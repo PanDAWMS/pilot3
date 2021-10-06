6c95526377c96472d35e1fc8a5056ee0f019dac3
Paul Nilsson <paul.nilsson@cern.ch>
2021-10-06 11:51:21 +0200

    Flake8 update

--- pilot/api/data.py
+++ pilot/api/data.py
@@ -1095,10 +1095,7 @@ class StageOutClient(StagingClient):
 
         # prepare files (resolve protocol/transfer url)
         if getattr(copytool, 'require_protocols', True) and files:
-            try:
-                output_dir = kwargs['output_dir']
-            except Exception:
-                output_dir = ""
+            output_dir = kwargs.get('output_dir', '')
             self.require_protocols(files, copytool, activity, local_dir=output_dir)
 
         if not copytool.is_valid_for_copy_out(files):
