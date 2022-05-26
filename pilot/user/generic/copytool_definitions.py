c2e008faa7556b0d5cc17e67b5334347322e4c37
Paul Nilsson <paul.nilsson@cern.ch>
2022-05-26 14:52:03 +0200

    Added copytool support module with mv_to_final_destination boolean function

---
+++ pilot/user/generic/copytool_definitions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Paul Nilsson, paul.nilsson@cern.ch, 2022
+
+
+def mv_to_final_destination():
+    """
+    Is mv allowed to move files to final destination?
+
+    :return: Boolean.
+    """
+
+    return False
