b5d27eed220150474ddfbb41165ac396e3a31e01
PalNilsson <Paul.Nilsson@cern.ch>
2022-05-27 11:19:38 +0200

    Clean up

--- pilot/util/monitoring.py
+++ pilot/util/monitoring.py
@@ -275,7 +275,6 @@ def verify_user_proxy(current_time, mt):
     proxy_verification_time = 60  # convert_to_int(config.Pilot.proxy_verification_time, default=600)
     if current_time - mt.get('ct_proxy') > proxy_verification_time:
         # is the proxy still valid?
-        logger.debug('calling verify_proxy with test=True')
         exit_code, diagnostics = userproxy.verify_proxy(test=False)  # use test=True to test expired proxy
         if exit_code != 0:
             return exit_code, diagnostics
