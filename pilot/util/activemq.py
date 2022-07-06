cafc0c337e20f3e6b48d3b5145a72425c5e99567
Paul Nilsson <paul.nilsson@cern.ch>
2022-07-06 16:23:59 +0200

    Working AMQ

--- pilot/util/activemq.py
+++ pilot/util/activemq.py
@@ -13,6 +13,8 @@ import logging
 import socket
 import sys
 import time
+import random
+import threading
 
 try:
     import stomp
@@ -24,6 +26,8 @@ from pilot.common.errorcodes import ErrorCodes
 
 logger = logging.getLogger(__name__)
 errors = ErrorCodes()
+
+graceful_stop = threading.Event()
 
 
 class MessagingListener(stomp.ConnectionListener):
@@ -46,7 +50,7 @@ class MessagingListener(stomp.ConnectionListener):
 
         :param frame:
         """
-        self.logger.error('[broker] [%s]: %s' % (self.__broker, frame))
+        self.logger.error(f'[broker] [{self.__broker}]: {frame}')
 
     def on_message(self, frame):
         """
@@ -54,21 +58,22 @@ class MessagingListener(stomp.ConnectionListener):
 
         :param frame:
         """
-        self.logger.info("received message: %s" % str(frame))
+        self.logger.info(f"received message: {frame}")
 
 
 class Receiver(stomp.ConnectionListener):
     """
     Messaging Receiver
     """
-    def __init__(self, broker):
+
+    messages = []
+
+    def __init__(self):
         """
         Init function.
-
-        :param broker:
         """
 
-        self.__broker = broker
+        self.__broker = None
         self.logger = logging.getLogger(self.__class__.__name__)
 
     def on_error(self, frame):
@@ -78,7 +83,7 @@ class Receiver(stomp.ConnectionListener):
         :param frame:
         """
 
-        self.logger.error('[broker] [%s]: %s', self.__broker, frame)
+        self.messages.append(f'ERROR: {frame.body}')
 
     def on_message(self, frame):
         """
@@ -87,11 +92,12 @@ class Receiver(stomp.ConnectionListener):
         :param frame:
         """
 
-        body = frame
-        self.logger.info("received message: %s", body)
-        body = json.loads(body.body)
-        if 'msg_type' in body and body['msg_type'] == 'health_heartbeat':
-            self.logger.info("received message: %s", body)
+        body = json.loads(frame.body)
+        self.logger.info(f"received message: {body} (broker={self.__broker})")
+        if body not in self.messages:
+            self.messages.append(body)
+
+        graceful_stop.set()
 
 
 class ActiveMQ(object):
@@ -105,10 +111,8 @@ class ActiveMQ(object):
     port = 0
     topic = '/topic/doma.panda_idds'
     receive_topics = [topic]
-    username = 'pilot'
-    password = 'trustno1'
-    ssl_key = ''
-    ssl_cert = ''
+    username = 'atlpndpilot'
+    password = ''
 
     def __init__(self, **kwargs):
         """
@@ -124,67 +128,70 @@ class ActiveMQ(object):
         self.receive_topics = kwargs.get('receive_topics', '')
         self.username = kwargs.get('username', '')
         self.password = kwargs.get('password', '')
-        self.ssl_key = kwargs.get('ssl_key', '')
-        self.ssl_cert = kwargs.get('ssl_cert', '')
-
+        self.connections = []
+        self.cid = kwargs.get('id', None)
+        self.vo = kwargs.get('vo', None)
         _addrinfos = socket.getaddrinfo(self.broker, 0, socket.AF_INET, 0, socket.IPPROTO_TCP)
         self.brokers_resolved = [_ai[4][0] for _ai in _addrinfos]
+
+        self.logger = logging.getLogger(self.__class__.__name__)
 
     def send_message(self):
         """
         Send a test message to the ActiveMQ queue.
         """
 
-        for broker in self.brokers_resolved:
-            if self.ssl_key and self.ssl_cert:
-                conn = stomp.Connection12(host_and_ports=[(broker, self.port)],
-                                          keepalive=True,
-                                          ssl_key_file=self.ssl_key,
-                                          ssl_cert_file=self.ssl_cert,
-                                          use_ssl=True)
-                conn.set_listener('message-sender', MessagingListener(conn.transport._Transport__host_and_ports[0]))
-            else:
-                conn = stomp.Connection12(host_and_ports=[(broker, self.port)],
-                                          keepalive=True,
-                                          use_ssl=False)
-                conn.set_listener('message-sender', MessagingListener(conn.transport._Transport__host_and_ports[0]))
-                conn.connect(self.username, self.password, wait=True)
+        broker = random.choice(self.brokers_resolved)
+        self.logger.info(f'will send message to random broker: {broker}')
+        conn = stomp.Connection12(host_and_ports=[(broker, self.port)], keepalive=True)
+        conn.set_listener('message-sender', MessagingListener(conn.transport._Transport__host_and_ports[0]))
+        conn.connect(self.username, self.password, wait=True)
 
-            # conn.start()
+        message = {'msg_type': 'get_job', 'taskid': 1234, 'jobid': 123456}
 
-            payload = {"files": [
-                {"adler32": "0cc737eb", "scope": "mock", "bytes": 1, "name": "file_ac6db48094974e039aa9d1ae52873596"}],
-                       "rse": "WJ-XROOTD", "operation": "add_replicas", "lifetime": 2}
-
-            logger.debug("sending message")
-            try:
-                for i in range(10):
-                    conn.send(destination=self.topic, body=json.dumps(payload), id='atlas-idds-messaging', ack='auto',
-                              headers={'persistent': 'true', 'vo': 'atlas'})
-            except Exception as ex:
-                logger.warning(ex)
-
-            logger.debug("sent message")
+        self.logger.info("sending message")
+        try:
+            conn.send(destination=self.topic,
+                      body=json.dumps(message),
+                      id=self.cid,
+                      ack='auto',
+                      headers={'persistent': 'true', 'vo': self.vo})
+        except Exception as exc:
+            self.logger.error(exc)
+        else:
+            self.logger.info("sent message")
 
     def receive_message(self):
         """
         Receive a message from ActiveMQ.
         """
 
-        for receive_topic in self.receive_topics:
-            print('start receiver: %s' % receive_topic)
-            if self.ssl_key and self.ssl_cert:
-                conn = stomp.Connection12(host_and_ports=[(self.broker, self.receiver_port)],
-                                          keepalive=True,
-                                          ssl_key_file=self.ssl_key,
-                                          ssl_cert_file=self.ssl_cert,
-                                          use_ssl=True)
-            else:
-                conn = stomp.Connection12(host_and_ports=[(self.broker, self.receiver_port)],
-                                          keepalive=True,
-                                          use_ssl=False)
-                conn.connect(self.username, self.password, wait=True)
-            conn.set_listener('message-receiver', Receiver(conn.transport._Transport__host_and_ports[0]))
+        receive_topic = self.receive_topics[0]
 
-            # conn.start()
-            conn.subscribe(destination=receive_topic, id='atlas-idds-messaging', ack='auto')
+        for broker in self.brokers_resolved:
+            conn = stomp.Connection12(host_and_ports=[(broker, self.receiver_port)],
+                                      keepalive=True)
+
+            if not conn in self.connections:
+                self.connections.append(conn)
+
+        receiver = Receiver()
+        messages = []
+        while not graceful_stop.is_set():
+            for conn in self.connections:
+                if not conn.is_connected():
+                    self.logger.info(f'connecting to {conn.transport._Transport__host_and_ports[0]}')
+                    receiver.set_broker(conn.transport._Transport__host_and_ports[0])
+                    conn.set_listener('message-receiver', receiver)
+                    conn.connect(self.username, self.password, wait=True)
+                    conn.subscribe(destination=receive_topic,
+                                   id=self.cid,
+                                   ack='auto')
+                    messages = receiver.get_messages()
+                    if messages:
+                        break
+
+            time.sleep(1)
+
+        self.close_connections()
+        return messages
