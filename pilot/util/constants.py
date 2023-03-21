a04bbf6db1cd3feb23a32e41dbbd0eb36e894ed2
Paul Nilsson <paul.nilsson@cern.ch>
2023-03-21 11:53:32 +0100

    Updated mod times

--- pilot/util/constants.py
+++ pilot/util/constants.py
@@ -6,7 +6,7 @@
 #
 # Authors:
 # - Mario Lassnig, mario.lassnig@cern.ch, 2017
-# - Paul Nilsson, paul.nilsson@cern.ch, 2018-2022
+# - Paul Nilsson, paul.nilsson@cern.ch, 2018-2023
 
 from os import environ
 
