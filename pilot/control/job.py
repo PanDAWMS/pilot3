d5b9715470ccfa93bf435b3f798b06c34b466df6
PalNilsson <Paul.Nilsson@cern.ch>
2023-05-08 16:18:13 +0200

    Removed abort

--- pilot/control/job.py
+++ pilot/control/job.py
@@ -2845,7 +2845,7 @@ def job_monitor(queues, traces, args):  # noqa: C901
                 logger.info('monitor loop #%d: job %d:%s is in state \'%s\'', n, i, current_id, jobs[i].state)
                 if jobs[i].state == 'finished' or jobs[i].state == 'failed':
                     logger.info('will abort job monitoring soon since job state=%s (job is still in queue)', jobs[i].state)
-                    abort = True
+                    # abort = True - do not set abort here as it will abort the entire thread, not just the current monitor loop
                     break
 
                 # perform the monitoring tasks
