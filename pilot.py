26e2e9bb6366df8c6c750e343a8b95069dfa088d
PalNilsson <Paul.Nilsson@cern.ch>
2023-06-01 17:14:56 +0200

    Pylint updates

--- pilot.py
+++ pilot.py
@@ -21,6 +21,7 @@ import time
 from os import getcwd, chdir, environ
 from os.path import exists, join
 from shutil import rmtree
+from typing import Any
 
 from pilot.common.errorcodes import ErrorCodes
 from pilot.common.exception import PilotException
@@ -593,7 +594,7 @@ def get_pilot_source_dir():
     return cwd
 
 
-def send_worker_status(status: str, queue: str, url: str, port: str, logger, internet_protocol_version: str):
+def send_worker_status(status: str, queue: str, url: str, port: str, logger: Any, internet_protocol_version: str):
     """
     Send worker info to the server to let it know that the worker has started
     Note: the function can fail, but if it does, it will be ignored.
