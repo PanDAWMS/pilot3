b9cd43beb7318c79a9fd7e4ac23d4b5faf337e8c
Paul Nilsson <palnilsson70@gmail.com>
2024-07-19 21:39:46 +0200

    Pylint updates

--- pilot/util/proxy.py
+++ pilot/util/proxy.py
@@ -44,38 +44,37 @@ def get_distinguished_name() -> str:
     executable = 'arcproxy -i subject'
     exit_code, stdout, stderr = execute(executable)
     if exit_code != 0 or "ERROR:" in stderr:
-        logger.warning("arcproxy failed: ec=%d, stdout=%s, stderr=%s" % (exit_code, stdout, stderr))
+        logger.warning(f"arcproxy failed: ec={exit_code}, stdout={stdout}, stderr={stderr}")
 
         if "command not found" in stderr or "Can not find certificate file" in stderr:
             logger.warning("arcproxy experienced a problem (will try voms-proxy-info instead)")
 
             # Default to voms-proxy-info
-            exit_code, stdout, stderr = vomsproxyinfo(options='-subject', mute=True)
+            exit_code, stdout, _ = vomsproxyinfo(options='-subject', mute=True)
 
     if exit_code == 0:
         dn = stdout
-        logger.info('DN = %s' % dn)
+        logger.info(f'DN = {dn}')
         cn = "/CN=proxy"
         if not dn.endswith(cn):
-            logger.info("DN does not end with %s (will be added)" % cn)
+            logger.info(f"DN does not end with {cn} (will be added)")
             dn += cn
 
     else:
-        logger.warning("user=self set but cannot get proxy: %d, %s" % (exit_code, stdout))
+        logger.warning(f"user=self set but cannot get proxy: {exit_code}, {stdout}")
 
     return dn
 
 
-def vomsproxyinfo(options='-all', mute=False, path=''):
+def vomsproxyinfo(options: str = '-all', mute: bool = False, path: str = '') -> tuple[int, str, str]:
     """
     Execute voms-proxy-info with the given options.
 
-    :param options: command options (string).
-    :param mute: should command output be printed (mute=False).
-    :param path: use given path if specified for proxy (string).
-    :return: exit code (int), stdout (string), stderr (string).
+    :param options: command options (str)
+    :param mute: should command output be printed (mute=False) or not (mute=True) (bool)
+    :param path: use given path if specified for proxy (str)
+    :return: exit code (int), stdout (string), stderr (str) (tuple).
     """
-
     executable = f'voms-proxy-info {options}'
     if path:
         executable += f' --file={path}'
@@ -86,7 +85,7 @@ def vomsproxyinfo(options='-all', mute=False, path=''):
     return exit_code, stdout, stderr
 
 
-def get_proxy(proxy_outfile_name: str, voms_role: str) -> (bool, str):
+def get_proxy(proxy_outfile_name: str, voms_role: str) -> tuple[bool, str]:
     """
     Download and store a proxy.
 
@@ -95,14 +94,14 @@ def get_proxy(proxy_outfile_name: str, voms_role: str) -> (bool, str):
 
     :param proxy_outfile_name: specify the file to store proxy (str)
     :param voms_role: what proxy (role) to request, e.g. 'atlas' (str)
-    :return: result (Boolean), updated proxy path (str).
+    :return: result (Boolean), updated proxy path (str) (tuple).
     """
     try:
         # it assumes that https_setup() was done already
         url = os.environ.get('PANDA_SERVER_URL', config.Pilot.pandaserver)
 
         pilot_user = os.environ.get('PILOT_USER', 'generic').lower()
-        user = __import__('pilot.user.%s.proxy' % pilot_user, globals(), locals(), [pilot_user], 0)
+        user = __import__(f'pilot.user.{pilot_user}.proxy', globals(), locals(), [pilot_user], 0)
         data = user.getproxy_dictionary(voms_role)
 
         res = https.request2(f'{url}/server/panda/getProxy', data=data)
@@ -123,12 +122,11 @@ def get_proxy(proxy_outfile_name: str, voms_role: str) -> (bool, str):
         logger.error(f"Get proxy from panda server failed: {exc}, {traceback.format_exc()}")
         return False, proxy_outfile_name
 
-    def create_file(filename, contents):
-        """
-        Internally used helper function to create proxy file.
-        """
+    def create_file(filename: str, contents: str) -> bool:
+        """Create a file with the given contents."""
         _file = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600)
         os.close(_file)
+
         return write_file(filename, contents, mute=False)  # returns True on success
 
     result = False
@@ -136,15 +134,15 @@ def get_proxy(proxy_outfile_name: str, voms_role: str) -> (bool, str):
         # pre-create empty proxy file with secure permissions. Prepare it for write_file() which can not
         # set file permission mode, it will write to the existing file with correct permissions.
         result = create_file(proxy_outfile_name, proxy_contents)
-    except (IOError, OSError, FileHandlingFailure) as exc:
+    except (OSError, FileHandlingFailure) as exc:
         logger.error(f"exception caught:\n{exc},\ntraceback: {traceback.format_exc()}")
         if 'Read-only file system' in exc:
             proxy_outfile_name = os.path.join(os.getenv('PILOT_HOME'), os.path.basename(proxy_outfile_name))  # e.g. '/path/x509up_u25606_prod-unified.proxy'
             logger.info(f'attempting writing proxy to alternative path: {proxy_outfile_name}')
             try:  # can we bypass a problem with read-only file systems by writing the proxy to the pilot home dir instead?
                 result = create_file(proxy_outfile_name, proxy_contents)
-            except (IOError, OSError, FileHandlingFailure) as exc:
-                logger.error(f"exception caught:\n{exc},\ntraceback: {traceback.format_exc()}")
+            except (OSError, FileHandlingFailure) as e:
+                logger.error(f"exception caught:\n{e},\ntraceback: {traceback.format_exc()}")
             else:
                 logger.debug('updating X509_USER_PROXY to alternative path {path} (valid until end of current job)')
                 os.environ['X509_USER_PROXY'] = proxy_outfile_name
@@ -155,16 +153,16 @@ def get_proxy(proxy_outfile_name: str, voms_role: str) -> (bool, str):
     return result, proxy_outfile_name
 
 
-def create_cert_files(from_proxy, workdir):
+def create_cert_files(from_proxy: str, workdir: str) -> tuple[str, str]:
     """
     Create cert/key pem files from given proxy and store in workdir.
+
     These files are needed for communicating with logstash server.
 
-    :param from_proxy: path to proxy file (string).
-    :param workdir: work directory (string).
-    :return: path to crt.pem (string), path to key.pem (string).
+    :param from_proxy: path to proxy file (str)
+    :param workdir: work directory (str)
+    :return: path to crt.pem (string), path to key.pem (string) (tuple).
     """
-
     _files = [os.path.join(workdir, 'crt.pem'), os.path.join(workdir, 'key.pem')]
     if os.path.exists(_files[0]) and os.path.exists(_files[1]):
         return _files[0], _files[1]
@@ -178,8 +176,8 @@ def create_cert_files(from_proxy, workdir):
         if ec:
             logger.warning(f'cert command failed: {stdout}, {stderr}')
             return '', ''
-        else:
-            logger.debug(f'produced key/cert file: {_files[counter]}')
-            counter += 1
+
+        logger.debug(f'produced key/cert file: {_files[counter]}')
+        counter += 1
 
     return _files[0], _files[1]
