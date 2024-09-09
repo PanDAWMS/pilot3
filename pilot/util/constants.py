df9a4d9ddd21c094430ab9a30707a8775d3e06ca
Paul Nilsson <palnilsson70@gmail.com>
2024-09-09 16:50:40 +0200

    New version

--- pilot/util/constants.py
+++ pilot/util/constants.py
@@ -27,8 +27,8 @@ from os import environ
 # Pilot version
 RELEASE = '3'   # released number should be fixed at 3 for Pilot 3
 VERSION = '8'   # version number is '1' for first release, '0' until then, increased for bigger updates
-REVISION = '1'  # revision number should be reset to '0' for every new version release, increased for small updates
-BUILD = '66'     # build number should be reset to '1' for every new development cycle
+REVISION = '2'  # revision number should be reset to '0' for every new version release, increased for small updates
+BUILD = '1'     # build number should be reset to '1' for every new development cycle
 
 SUCCESS = 0
 FAILURE = 1
