e20a831a3714fd7492e2d9df108fdc1346b5411f
Paul Nilsson <palnilsson70@gmail.com>
2024-11-01 15:55:52 +0100

    Pylint updates

--- pilot/user/rubin/loopingjob_definitions.py
+++ pilot/user/rubin/loopingjob_definitions.py
@@ -20,29 +20,30 @@
 # - Paul Nilsson, paul.nilsson@cern.ch, 2018-24
 
 
-def allow_loopingjob_detection():
+def allow_loopingjob_detection() -> bool:
     """
-    Should the looping job detection algorithm be allowed?
+    Decide if the looping job detection algorithm should be allowed.
+
     The looping job detection algorithm finds recently touched files within the job's workdir. If a found file has not
     been touched during the allowed time limit (see looping job section in util/default.cfg), the algorithm will kill
     the job/payload process.
 
-    :return: boolean.
+    :return: True if yes (bool).
     """
-
     return True
 
 
-def remove_unwanted_files(workdir, files):
+def remove_unwanted_files(workdir: str, files: list) -> list:
     """
     Remove files from the list that are to be ignored by the looping job algorithm.
 
-    :param workdir: working directory (string). Needed in case the find command includes the workdir in the list of
-    recently touched files.
-    :param files: list of recently touched files (file names).
-    :return: filtered files list.
-    """
+    The workdir is needed in case the find command includes the workdir
+    in the list of  recently touched files.
 
+    :param workdir: working directory (str)
+    :param files: list of recently touched files (list)
+    :return: filtered files (list).
+    """
     _files = []
     for _file in files:
         if not (workdir == _file or
