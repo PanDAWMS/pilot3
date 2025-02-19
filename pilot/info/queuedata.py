636c0acff472da54f3a768850b17f5939b2789b4
Paul Nilsson <palnilsson70@gmail.com>
2025-02-19 09:11:15 +0100

    Updated default pilot_rss_grace from 1 to 2

--- pilot/info/queuedata.py
+++ pilot/info/queuedata.py
@@ -98,7 +98,7 @@ class QueueData(BaseData):
 
     altstageout = None  # allow altstageout: force (True) or disable (False) or no preferences (None)
     pilot_walltime_grace = 1.0  # pilot walltime grace factor
-    pilot_rss_grace = 1.0  # pilot rss grace factor
+    pilot_rss_grace = 2.0  # pilot rss grace factor
 
     # specify the type of attributes for proper data validation and casting
     _keys = {int: ['timefloor', 'maxwdir', 'pledgedcpu', 'es_stageout_gap',
