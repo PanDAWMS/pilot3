09a66ff8382352662f3383403f1c2b7d472e3db3
Paul Nilsson <palnilsson70@gmail.com>
2025-07-26 12:57:45 +0200

    Testing get_resource_types()

--- pilot/util/https.py
+++ pilot/util/https.py
@@ -1256,11 +1256,13 @@ def get_resource_types(url: str, port: int) -> dict:
     _resource_types = response.get('resourceTypes', {})
     # Convert Python-style string to JSON-style string for parsing
     resource_types_str = _resource_types.get('ResourceTypes')
+    logger.debug("resource_types_str: {}".format(resource_types_str))
     if resource_types_str:
         resource_types_str = resource_types_str.replace("None", "null").replace("'", '"')
         resource_types_list = json.loads(resource_types_str)
     else:
         resource_types_list = []
+    logger.debug("resource_types_list: {}".format(resource_types_list))
     resource_types = {}
     for entry in resource_types_list:
         resource_name = entry.get('resource_name')
