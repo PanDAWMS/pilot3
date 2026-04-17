b931d3aaad960d41763f6feaaacaedb888aec182
Paul Nilsson <palnilsson70@gmail.com>
2026-04-17 16:53:18 +0200

    Added removal of redundant files

--- pilot/user/epic/common.py
+++ pilot/user/epic/common.py
@@ -25,6 +25,7 @@ import logging
 import os
 import re
 
+from glob import glob
 from signal import SIGTERM
 from typing import Any
 
@@ -42,9 +43,12 @@ from pilot.util.constants import (
     UTILITY_BEFORE_STAGEIN,
     UTILITY_WITH_PAYLOAD,
 )
+from pilot.util.container import execute
 from pilot.util.filehandling import (
     get_guid,
-    read_file
+    read_file,
+    remove,
+    remove_dir_tree,
 )
 from pilot.util.https import get_base_urls
 
@@ -214,21 +218,254 @@ def validate_output_data(job: JobData) -> None:
             logger.warning(f'guid not set: generated guid={dat.guid} for lfn={dat.lfn}')
 
 
-def remove_redundant_files(workdir: str, outputfiles: list = None, piloterrors: list = None, debugmode: bool = False) -> None:
-    """Remove redundant files and directories prior to creating the log file.
+def get_redundant_path() -> str:
+    """Return the path to the file containing the redundant files and directories to be removed prior to log file creation.
+
+    Returns:
+        str: file path.
+    """
+    filename = config.Pilot.redundant
+
+    # correct /cvmfs if necessary
+    if filename.startswith('/cvmfs') and os.environ.get('ATLAS_SW_BASE', False):
+        filename = filename.replace('/cvmfs', os.environ.get('ATLAS_SW_BASE'))
+
+    return filename
+
+
+def get_redundants() -> list:
+    """Get list of redundant files and directories (to be removed).
+
+    The function will return the content of an external file. It that
+    can't be read, then a list defined in this function will be returned instead.
+    Any updates to the external file must be propagated to this function.
+
+    Returns:
+        list: files and directories.
+    """
+    # try to read the list from the external file
+    filename = get_redundant_path()
+
+    # do not use the cvmfs file since it is not being updated
+    # If you uncomment this block, need to also uncomment the read_list import
+    # if os.path.exists(filename) and False:
+    #    dir_list = read_list(filename)
+    #    if dir_list:
+    #        return dir_list
+
+    logger.debug(f'list of redundant files could not be read from external file: {filename} (will use internal list)')
+
+    # else return the following
+    dir_list = [".asetup.save",
+                "AtlasProduction*",
+                "AtlasPoint1",
+                "AtlasTier0",
+                "buildJob*",
+                "CDRelease*",
+                "ckpt*",
+                "csc*.log",
+                "DBRelease*",
+                "EvgenJobOptions",
+                "external",
+                "fort.*",
+                "geant4",
+                "geomDB",
+                "geomDB_sqlite",
+                "home",
+                "LICENSE",
+                "madevent",
+                "o..pacman..o",
+                "pacman-*",
+                "python*",
+                "requirements.txt",
+                "runAthena*",
+                "runGen-*",
+                "scratch",
+                "setup.cfg",
+                "share",
+                "sources.*",
+                "sqlite*",
+                "sw",
+                "stage*.sh",
+                "tcf_*",
+                "triggerDB",
+                "trusted.caches",
+                "workdir",
+                "*.data*",
+                "*.events",
+                "*.py",
+                "*.pyc",
+                "*.root*",
+                "tmp*",
+                "*.tmp",
+                "*.TMP",
+                "*.writing",
+                "pwg*",
+                "pwhg*",
+                "*PROC*",
+                "*proxy",
+                "*runcontainer*",
+                "*job.log.tgz",
+                "pandawnutil",
+                "src",
+                "singularity_cachedir",
+                "apptainer_cachedir",
+                "_joproxy15",
+                "HAHM_*",
+                "Process",
+                "merged_lhef._0.events-new",
+                "panda_secrets.json",
+                "singularity",
+                "apptainer",
+                "work",
+                "PILOTVERSION",
+                "README*",
+                "CLAUDE.md",
+                "MANIFEST*",
+                "*.part*",
+                "__pycache__*",
+                "x509*",
+                "docs",
+                "venv",
+                "usr",
+                "%1",
+                "open_remote_file_cmd.sh"]
+
+    return dir_list
+
+
+def remove_special_files(workdir: str, dir_list: list) -> None:
+    """Remove list of special files from the workdir.
+
+    Args:
+        workdir: work directory.
+        dir_list: list of special files.
+    """
+    # note: these should be partial file/dir names, not containing any wildcards
+    exceptions_list = ["runargs", "runwrapper", "jobReport", "log.", "xrdlog"]
+
+    to_delete = []
+    for _dir in dir_list:
+        files = glob(os.path.join(workdir, _dir))
+        if not files:
+            continue
+
+        exclude = []
+        for exc in exceptions_list:
+            for item in files:
+                if exc in item:
+                    exclude.append(os.path.abspath(item))
+
+        _files = [os.path.abspath(item) for item in files if item not in exclude]
+        to_delete += _files
+
+    for item in to_delete:
+        if os.path.isfile(item):
+            remove(item)
+        else:
+            remove_dir_tree(item)
+
+
+def cleanup_looping_payload(workdir: str) -> None:
+    """Run a special cleanup for looping payloads.
+
+    Remove any root and tmp files.
 
     Args:
         workdir: working directory.
-        outputfiles: list of output files.
-        piloterrors: list of Pilot assigned error codes.
-        debugmode: debug mode.
     """
-    if workdir or outputfiles or piloterrors or debugmode:  # to bypass pylint score 0
-        pass
-    #if outputfiles is None:
-    #    outputfiles = []
-    #if piloterrors is None:
-    #    piloterrors = []
+    for (root, _, files) in os.walk(workdir):
+        for filename in files:
+            if 'pool.root' in filename:
+                path = os.path.join(root, filename)
+                path = os.path.abspath(path)
+                remove(path)
+
+
+def cleanup_broken_links(workdir: str) -> None:
+    """Run a second pass to clean up any broken links prior to log file creation.
+
+    Args:
+        workdir: working directory.
+    """
+    broken = []
+    for root, _, files in os.walk(workdir):
+        for filename in files:
+            path = os.path.join(root, filename)
+            if not os.path.islink(path):
+                continue
+
+            target_path = os.readlink(path)
+            # Resolve relative symlinks
+            if not os.path.isabs(target_path):
+                target_path = os.path.join(os.path.dirname(path), target_path)
+            if not os.path.exists(target_path):
+                broken.append(path)
+
+    for brok in broken:
+        remove(brok)
+
+
+def remove_redundant_files(workdir: str, outputfiles: list = None, piloterrors: list = None, debugmode: bool = False) -> None:
+    """Remove redundant files and directories prior to creating the log file.
+
+    Note: in debug mode, any core files should not be removed before creating the log.
+
+    Args:
+        workdir: working directory.
+        outputfiles: list of protected output files.
+        piloterrors: list of Pilot assigned error codes.
+        debugmode: True if debug mode has been switched on.
+    """
+    if outputfiles is None:
+        outputfiles = []
+    if piloterrors is None:
+        piloterrors = []
+    logger.debug("removing redundant files prior to log creation")
+    workdir = os.path.abspath(workdir)
+
+    # remove special files
+    # get list of redundant files and directories (to be removed)
+    dir_list = get_redundants()
+
+    remove_special_files(workdir, dir_list)
+
+    # verify_container_script(os.path.join(workdir, config.Container.container_script))
+
+    # run a second pass to clean up any broken links
+    cleanup_broken_links(workdir)
+
+    # remove any present user workDir
+    path = os.path.join(workdir, 'workDir')
+    if os.path.exists(path):
+        # remove at least root files from workDir (ie also in the case of looping job)
+        cleanup_looping_payload(path)
+        islooping = errors.LOOPINGJOB in piloterrors
+        ismemerror = errors.PAYLOADEXCEEDMAXMEM in piloterrors
+        if not islooping and not ismemerror:
+            logger.debug(f'removing \'workDir\' from workdir={workdir}')
+            remove_dir_tree(path)
+
+    # remove additional dirs
+    additionals = ['singularity', 'pilot', 'cores']
+    for additional in additionals:
+        path = os.path.join(workdir, additional)
+        if os.path.exists(path):
+            logger.debug(f"removing \'{additional}\' from workdir={workdir}")
+            remove_dir_tree(path)
+
+    list_work_dir(workdir)
+
+
+def list_work_dir(workdir: str) -> None:
+    """Execute ls -lF for the given directory and dump to log.
+
+    Args:
+        workdir: directory name.
+    """
+    cmd = f'ls -lF {workdir}'
+    _, stdout, stderr = execute(cmd)
+    logger.debug(f'{stdout}:\n' + stderr)
 
 
 def get_utility_commands(order: int = None, job: JobData = None, base_urls: list = None) -> dict:
